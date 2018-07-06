e13461f981729389e50e800f803096f143f53968
nanzheng <nanzheng@freedotech.com>
2018-07-06 10:19:58 +0800

    lalala

---
+++ src/main/java/com/nz/springbootlearning/Note/BinaryTree.java
@@ -0,0 +1,76 @@
+package com.nz.springbootlearning.Note;
+
+/**
+ * @ Author     ：nz.
+ * @ Date       ：Created in 上午 9:57 18/7/6
+ * @ Description：二叉树遍历
+ * @ Modified By：
+ * @Version: $
+ */
+public class BinaryTree {
+    public Note init(){
+        Note J = new Note(8, null, null);
+        Note H = new Note(4, null, null);
+        Note G = new Note(2, null, null);
+        Note F = new Note(7, null, J);
+        Note E = new Note(5, H, null);
+        Note D = new Note(1, null, G);
+        Note C = new Note(9, F, null);
+        Note B = new Note(3, D, E);
+        Note A = new Note(6, B, C);
+        return A;   //返回根节点  
+    }
+    public  void  printNode(Note note){
+        System.out.println(note.getVal());
+    }
+    /**
+     * create by: nz
+     * description:先序遍历
+     * create time: 上午 10:13 18/7/6
+     *
+     * @param note
+     * @return void
+     */
+    public void theFirstTraversal(Note note){
+
+        printNode(note);
+        if (note.getLeftNode()!=null){
+            theFirstTraversal(note.getLeftNode());}
+        if (note.getRightNode()!= null){
+            theFirstTraversal(note.getRightNode());
+        }
+    }
+
+    /**
+     * create by: nz
+     * description:中序遍历
+     * create time: 上午 10:17 18/7/6
+     *
+      * @param
+     * @return
+     */
+        public void thInorderTraversal(Note note){
+        if (note.getLeftNode()!=null){
+            theFirstTraversal(note.getLeftNode());}
+        printNode(note);
+        if (note.getRightNode()!= null){
+            theFirstTraversal(note.getRightNode());
+        }
+    }  /**
+     * create by: nz
+     * description:后序遍历
+     * create time: 上午 10:17 18/7/6
+     *
+      * @param
+     * @return
+     */
+        public void thLastTraversal(Note note){
+        if (note.getLeftNode()!=null){
+            theFirstTraversal(note.getLeftNode());}
+
+        if (note.getRightNode()!= null){
+            theFirstTraversal(note.getRightNode());
+        }
+            printNode(note);
+    }
+}
--- src/main/java/com/nz/springbootlearning/Note/Note.java
+++ src/main/java/com/nz/springbootlearning/Note/Note.java
@@ -8,4 +8,38 @@ package com.nz.springbootlearning.Note;
  * @Version: $
  */
 public class Note {
+
+    private int val;
+    private Note leftNode;
+    private Note rightNode;
+
+    public Note(int val, Note leftNode, Note rightNode) {
+        this.val = val;
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public Note getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(Note leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public Note getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(Note rightNode) {
+        this.rightNode = rightNode;
+    }
 }
